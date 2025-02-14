c8e27dd2ca7041e1405f3c9281dde26fb00c5e07
jayce <jayce.baxter@proton.me>
2025-02-14 08:56:37 -0500

    adding values to arrays

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,92 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Initializes scanner and prints menu options
+        // Initializes scanner
         Scanner myScanner = new Scanner(System.in);
-        System.out.print("1. Add temperature\n2. Move on to next day\n3. Calculate\n4. Exit program");
 
         // Initializing the menu option because it was yelling at me
         int menuOption = 0;
 
-        // Prompts user for menu input
         do {
-            menuOption = myScanner.nextInt();
 
-            // If user inputs 1, allows them to add a temperature
-            if (menuOption == 1) {
+            // Prints menu options
+            System.out.print("1. Add temperature\n2. Move on to next day\n3. Calculate\n4. Exit program");
+            System.out.print("\nPlease select: ");
 
-                // Asks user how many days they would like to calculate
-                System.out.print("How many days would you like to input?");
-                int numDays = myScanner.nextInt();
+            // If the scanner holds an int, assigns value to menuOption
+            if (myScanner.hasNextInt()){
+                menuOption = myScanner.nextInt();
 
-                // Initializes 2 lists that are the size of numDays
-                double[] lowTemp = new double[numDays];
-                double[] highTemp = new double[numDays];
+                // If user inputs 1, allows them to add a temperature
+                if (menuOption == 1) {
 
-                // Filling the arrays with a random value and praying the user doesn't input it
-                Arrays.fill(highTemp, 3.14159);
-                Arrays.fill(lowTemp, 3.14159);
+                    // Asks user how many days they would like to calculate
+                    System.out.print("How many days would you like to input?");
+                    if (myScanner.hasNextInt()){
+                        int numDays = myScanner.nextInt();
 
-                // Prompts user for the high temp of the day
-                System.out.print("Please enter the highest temperature of the day: ");
-                double hiTempInput = myScanner.nextDouble();
+                        // Initializes 2 lists that are the size of numDays
+                        double[] lowTemp = new double[numDays];
+                        double[] highTemp = new double[numDays];
 
-                // Prompts user for the low temp of the day
-                System.out.print("Please enter the lowest temperature of the day: ");
-                double lowTempInput = myScanner.nextDouble();
+                        // Initializing variables to use to add to the arrays
+                        double hiTempInput;
+                        double lowTempInput;
 
+                        // Filling the arrays with a random value and praying the user doesn't input it
+                        Arrays.fill(highTemp, 3.14159);
+                        Arrays.fill(lowTemp, 3.14159);
+
+                        // Prompts user for the high temp of the day
+                        System.out.print("Please enter the highest temperature of the day: ");
+                        hiTempInput = myScanner.nextDouble();
+
+                        // If the input isn't between -45 and 45, prints error and reprompts
+                        if (hiTempInput > 45 || hiTempInput < -45){
+                            System.out.println("Input must be between 45 and -45 degrees");
+                            System.out.print("Please enter the highest temperature of the day: ");
+                            hiTempInput = myScanner.nextDouble();
+                        }
+
+                        else{
+                            for(int i = 0; i < highTemp.length; i++)
+                                if (highTemp[i] == 3.14159) {
+                                    highTemp[i] = hiTempInput;
+                                }
+                        }
+
+                        System.out.println(highTemp);
+
+                        // Prompts user for the low temp of the day
+                        System.out.print("Please enter the lowest temperature of the day: ");
+                        lowTempInput = myScanner.nextDouble();
+
+
+                    }
+
+                    // Prints an error message and clears the scanner if input is invalid
+                    // Stole this because I didn't know how to do it and made an infinite loop
+                    // If this isn't the right way to do it, I'd love to know what is
+                    // https://stackoverflow.com/questions/10604125/how-can-i-clear-the-scanner-buffer-in-java
+                    else {
+                        System.out.println("Please enter a whole number.");
+                        myScanner.nextLine();
+                    }
+
+
+                }
 
             }
+
+
+
+            // Prints an error message and clears the scanner if input is invalid
+            else {
+                System.out.println("Please input a whole number.");
+                myScanner.nextLine();
+            }
+
+
 
             if (menuOption == 2) {
                 System.out.print("Max for the day:\nMin for the day:\nAvg for the day:");
